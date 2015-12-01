6e5ac420a069b234bbc80d4a6579c9e309dde393
tskinn <tskinn12@gmail.com>
2015-11-30 21:16:32 -0700

    google places autocomplete working

---
+++ app/scripts/controllers/my.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name protoMnKApp.controller:MyctrlCtrl
+ * @description
+ * # MyctrlCtrl
+ * Controller of the protoMnKApp
+ */
+angular.module('protoMnKApp')
+    .controller('MyCtrl', function ($scope) {
+        $scope.gPlace;
+    });
---
+++ app/scripts/directives/googleplace.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name protoMnKApp.directive:googleplace
+ * @description
+ * # googleplace
+ */
+angular.module('protoMnKApp')
+    .directive('googleplace', function () {
+        return {
+            require: 'ngModel',
+            scopde: {
+                ngModel: '=',
+                details: '=?'
+            },
+            link: function(scope, element, attrs, model) {
+                var options = {
+                    types: [],
+                    componentRestrictions: {}
+                };
+                scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+                google.maps.event.addListener(scope.gPlace, 'place_changed', function(){
+                    scope.$apply(function() {
+                        scope.details = scope.gPlace.getPlace();
+                        model.$setViewValue(element.val());
+                    });
+                });
+            }
+            // template: '<div></div>',
+            // restrict: 'E',
+            // link: function postLink(scope, element, attrs) {
+            //     element.text('this is the googleplace directive');
+            // }
+        };
+    });
+
+// function MyCtrl($scope) {
+//     $scope.gPlace;
+// }
