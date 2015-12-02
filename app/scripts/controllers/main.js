aab6a39fc8350215af9fe51278f5ba720c6af879
Taylor Skinner <tskinner@kyanicorp.com>
2015-12-02 13:59:57 -0700

    almost got database running

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,24 @@ angular.module('protoMnKApp')
         $scope.gPlace;
         $scope.user = user.google.displayName;
 
+        var ref = new Firebase("https://blazing-torch-6786.firebaseio.com/addresses");
+
         $scope.logout = function() { Auth.$unauth(); };
-//        $scope.rating = "";
+        $scope.rating = "";
+
+        $scope.address = {
+            rainy: 0,
+            cloudy: 0,
+            sunny: 0
+        }
+        
+        $scope.addressGet = function() {
+            if ($scope.chosenPlace) {
+                var address = $firebaseObject(ref.child($scope.chosenPlace));
+                address.$bindTo($scope, 'address');
+                $scope.rainys = address.rainy
+                $scope.cloudys = address.cloudy
+                $scope.sunnys = address.sunny
+            }
+        };
     });
